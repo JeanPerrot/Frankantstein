b3230cf2a548a6be10e96965c582d309bb58ffac
Jean Perrot <jean.brahnperrot@gmail.com>
2011-11-17 12:24:26 -0800

    cleaning up unused files

--- tutorial.sh
+++
@@ -1,1 +0,0 @@
-python tools/playgame.py "java -jar build/libs/MyBot.jar" "java -jar fightBot-v1.jar" --map_file tools/maps/maze/maze_02p_01.map --log_dir game_logs --turns 1000 --player_seed 7 --verbose -e
